6d97253d681c64008bff15e3df694051e811dc00
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-10-01 23:44:44 +0530

    Update cont based recom.py

--- cont based recom.py
+++ cont based recom.py
@@ -18,7 +18,7 @@ df["all_X"]= df.apply(all_X,axis=1)
 cv=CountVectorizer()
 count_matrix=cv.fit_transform(df["all_X"])#The fit() function calculates the values of these parameters. The transform function applies the values of the parameters on the actual data and gives the normalized value. The fit_transform() function performs both in the same step. Note that the same value is got whether we perform in 2 steps or in a single step.
 cosine=cosine_similarity(count_matrix) 
-movie_user_likes="Avatar"#movie in accordance to which recommendation should be given
+movie_user_likes=input("Enter the movie you like :")#movie in accordance to which recommendation should be given
 movie_index=index_title(movie_user_likes)
 similar_movies=list(enumerate(cosine[movie_index]))#finding the cosine similarity of the movies
 sorted_similar_movies=sorted(similar_movies,key=lambda x:x[1],reverse=True)#we will get cosine similarity in ascending order therefore we need to reverse it to get vlues desirably and we'd remove the first as if cosine distance is 1 it would be the same movie which the user like
